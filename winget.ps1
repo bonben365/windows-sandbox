ae725ea7c5c69340f11dabbe7ce99a71b9ab7153
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-08 19:54:48 +0700

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -10,6 +10,11 @@ For detailed script execution: https://bonben365.com/
 =================================================================================================================
 #>
 
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    break
+}
+
 # Create temporary directory
 $null = New-Item -Path $env:temp\temp -ItemType Directory -Force
 Set-Location $env:temp\temp
